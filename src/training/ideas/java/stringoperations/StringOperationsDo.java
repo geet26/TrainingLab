f3de0a179bef0c76d1fd4be1fd12a64763ee6555
geet26 <geetanjali.belsare@gmail.com>
2014-08-14 10:30:55 +0530

    Program for String combinations

---
+++ src/training/ideas/java/stringoperations/StringOperationsDo.java
@@ -0,0 +1,22 @@
+package training.ideas.java.stringoperations;
+
+/**
+ * Created by idngeb on 2014-08-13.
+ */
+public class StringOperationsDo {
+    public static String[] find(String input) {
+        String strArr[] = new String[input.length()];
+        if(input.length()==0){
+            return  strArr;
+        }
+        else{
+            strArr[0] = input;
+            for (int i = 1; i < input.length() ; i++) {
+                strArr[i] = input.substring(1,input.length()) + input.substring(0,1);
+                input = strArr[i];
+            }
+            return strArr;
+        }
+
+    }
+}
